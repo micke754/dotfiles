567d9edcd529b6dece74cd3aea58e2e43e967704
Kevin Michael <kmichaels@Kevins-MacBook-Air.local>
2025-06-01 21:27:31 +1200

    Minor changes to wezterm and helix theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,7 +11,7 @@ local config = wezterm.config_builder()
 -- config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
-config.font_size = 18
+config.font_size = 16
 config.font = wezterm.font("FiraCode Nerd Font Propo")
 -- config.font = wezterm.font("IosevkaTerm Nerd Font Propo")
 -- config.color_scheme = "kanagawabones"
